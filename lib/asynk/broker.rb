9713c98d266ed7a7ca2452ca46404eb04c939cbf
Danil Nurgaliev <jkonalegi@gmail.com>
2015-09-05 20:36:05 +0300

    add connection options to session

--- lib/asynk/broker.rb
+++ lib/asynk/broker.rb
@@ -9,7 +9,12 @@ module Asynk
     class << self
       def amqp_connection
         @amqp_connection ||= begin
-          conn = Bunny.new
+          conn = Bunny.new(host: Asynk.config[:mq_host],
+                           port: Asynk.config[:mq_port],
+                       username: Asynk.config[:mq_username],
+                       password: Asynk.config[:mq_password],
+                          vhost: Asynk.config[:mq_vhost])
+          p conn
           conn.start
           conn
         end

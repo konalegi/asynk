924b563ed1386d2e90c1ad8cba247b8e7ce49a92
Danil Nurgaliev <jkonalegi@gmail.com>
2015-09-07 20:03:42 +0300

    remote comments

--- lib/asynk/consumer.rb
+++ lib/asynk/consumer.rb
@@ -35,19 +35,15 @@ module Asynk
     module ClassMethods
       attr_reader :routing_keys, :queue_name, :subscribe_arguments, :queue_options
 
-      # Add one or more routing keys to the set of routing keys the consumer
-      # wants to subscribe to.
       def set_consume(*routing_keys)
         @routing_keys = routing_keys
       end
 
-      # Explicitly set the queue name
       def set_queue(name, options = {})
         @queue_name = name
         @queue_options = options
       end
 
-      # Allow to specify custom arguments that will be passed when creating the queue.
       def set_subscribe_arguments(arguments = {})
         @subscribe_arguments = arguments
       end

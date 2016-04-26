6e064e1b2f68f52c6800120b537e35f1e3629d71
Danil Nurgaliev <jkonalegi@gmail.com>
2016-04-26 17:15:31 +0300

    add benchmark methods

--- lib/asynk/benchmark.rb
+++ lib/asynk/benchmark.rb
@@ -1,11 +1,21 @@
 module Asynk
   class Benchmark
     class << self
+
       def measure_around(&block)
         start_time = Time.now.to_f
         block.call
         ((Time.now.to_f - start_time)*1000).round(2)
       end
+
+      def start
+        Time.now.to_f
+      end
+
+      def end(start_time)
+        ((Time.now.to_f - start_time)*1000).round(2)
+      end
+
     end
   end
 end
--- lib/asynk/worker.rb
+++ lib/asynk/worker.rb
@@ -25,13 +25,13 @@ module Asynk
     end
 
     def on_event(delivery_info, properties, payload)
-      start_time = Time.now.to_f
+      global_start_time =  Asynk::Benchmark.start
       message = Asynk::Message.new(delivery_info, properties, payload)
       consumer_instance = @consumer.new(@ch, delivery_info) do |result|
         @default_exchange.publish(convert_to_valid_json_string(result), routing_key: properties.reply_to, correlation_id: properties.correlation_id)
 
         if Asynk.config[:respond_back_execution_time]
-          Asynk.logger.info "Responded to message #{message.routing_key}:#{message.message_id} In: #{((Time.now.to_f - start_time)*1000).round(2)} ms."
+          Asynk.logger.info "Responded to message #{message.routing_key}:#{message.message_id} In: #{Asynk::Benchmark.end(global_start_time)} ms."
         end
 
         Asynk.logger.debug "#{@consumer.name}:#{@instance_id} Respoding to message id: #{message.message_id} with: #{result.to_s}. "

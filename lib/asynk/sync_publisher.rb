5a2b73077416f92f283b91c6028c8d66237f28cd
Danil Nurgaliev <jkonalegi@gmail.com>
2016-04-26 17:27:49 +0300

    add timeout implementation

--- lib/asynk/sync_publisher.rb
+++ lib/asynk/sync_publisher.rb
@@ -4,7 +4,7 @@ module Asynk
       @routing_key    = routing_key
       @params         = params
       @message_id     = (@params.delete(:message_id) || generate_message_id)
-      @wait_timeout   = (@params.delete(:timeout) || Asynk.config[:sync_publish_wait_timeout])
+      @wait_timeout   = (@params.delete(:timeout) || Asynk.config[:sync_publish_wait_timeout]) * 1000
       @correlation_id = generate_message_id
     end
 
@@ -13,9 +13,11 @@ module Asynk
       Asynk.broker.pool.with do |channel, exchange, reply_queue|
         exchange.publish(@params.to_json, message_id: @message_id, routing_key: @routing_key, correlation_id: @correlation_id, reply_to: reply_queue.name)
 
+        start_time = Asynk::Benchmark.start
         while !@response do
           delivery_info, properties, payload = reply_queue.pop
           @response = payload if payload && properties[:correlation_id] == @correlation_id
+          raise(RuntimeError.new('Timeout error reached')) if @wait_timeout <= Asynk::Benchmark.end(start_time)
         end
       end
 
